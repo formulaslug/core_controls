ba4cd29296fdcf95aa784292d13e79fd7e07f936
Tyler Veness <calcmogul@gmail.com>
2016-09-30 19:33:30 -0700

    Constants now conform to Google style guide

--- CANopen.h
+++ CANopen.h
@@ -9,14 +9,14 @@
 #include "CircularBuffer.h"
 
 // COB-IDs: MAX LENGTH of 12 bits, only the LSB 12 should be used
-constexpr uint32_t cobid_TPDO5 = 0x241;  // including throttle voltage payload
-constexpr uint32_t cobid_node3Heartbeat = 0x003;
-constexpr uint32_t cobid_node4Heartbeat = 0x004;
-constexpr uint32_t cobid_p2s = 0x013;
-constexpr uint32_t cobid_s2p = 0x014;
+constexpr uint32_t kCobid_TPDO5 = 0x241;  // including throttle voltage payload
+constexpr uint32_t kCobid_node3Heartbeat = 0x003;
+constexpr uint32_t kCobid_node4Heartbeat = 0x004;
+constexpr uint32_t kCobid_p2s = 0x013;
+constexpr uint32_t kCobid_s2p = 0x014;
 
 // Payload constants
-constexpr uint32_t payload_heartbeat = 0x1;
+constexpr uint32_t kPayloadHeartbeat = 0x1;
 
 class CANopen : public FlexCAN {
  public:
